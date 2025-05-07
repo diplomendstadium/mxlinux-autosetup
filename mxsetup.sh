e69da9c7ee22553173a90cd5ed796c4a73d06c2f
DiplomEndStadium <159532008+diplomendstadium@users.noreply.github.com>
2025-05-07 17:56:21 +0200

    Update mxsetup.sh

--- mxsetup.sh
+++ mxsetup.sh
@@ -16,27 +16,27 @@ echo " "
 # Update the System
 echo "[Info] Updating the system..."
 sudo apt update
-sudo apt full-upgade -y
-flatpack update -y
+sudo apt full-upgrade -y
+flatpak update -y
 
 # Installing Packages
 echo "[Info] Installing Software..."
 sudo apt update
-sudo apt install -y /
-    borgbackup /
-    ffmpeg /
-    firefox-l10n-xpi-de /
-    gimp /
-    gnome-disk-utility /
-    hunspell-de-de /
-    idle /
-    keepassxc /
-    kleopatra /
-    magic-wormhole /
-    onionshare onionshare-cli /
-    thunderbird-l10n-de /
-    tldr /
-    tor torsocks/
+sudo apt install -y \
+    borgbackup \
+    ffmpeg \
+    firefox-l10n-xpi-de \
+    gimp \
+    gnome-disk-utility \
+    hunspell-de-de \
+    idle \
+    keepassxc \
+    kleopatra \
+    magic-wormhole \
+    onionshare onionshare-cli \
+    thunderbird-l10n-de \
+    tldr \
+    tor torsocks \
     vim
 
 # Install further software via flatpak
