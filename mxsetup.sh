362c9876dfe22a318fa4ef3f467620bf7177bfb3
DiplomEndStadium <159532008+diplomendstadium@users.noreply.github.com>
2025-05-07 17:44:45 +0200

    Update mxsetup.sh

--- mxsetup.sh
+++ mxsetup.sh
@@ -17,17 +17,41 @@ echo " "
 echo "[Info] Updating the system..."
 sudo apt update
 sudo apt full-upgade -y
+flatpack update -y
 
 # Installing Packages
 echo "[Info] Installing Software..."
 sudo apt update
 sudo apt install -y /
+    borgbackup /
+    ffmpeg /
+    firefox-l10n-xpi-de /
+    gimp /
+    gnome-disk-utility /
+    hunspell-de-de /
+    idle /
+    keepassxc /
+    kleopatra /
+    magic-wormhole /
+    onionshare onionshare-cli /
+    thunderbird-l10n-de /
+    tldr /
+    tor torsocks/
+    vim
+
+# Install further software via flatpak
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak install -y /
+    org.torproject.torbrowser-launcher /
+    org.onlyoffice.desktopeditors
     
 
 # Activate and enable UFW firewall
 #echo "[INFO] Activate the firewall..."
-#sudo ufw enable
-#sudo systemctl enable ufw
+#sudo ufw default deny incoming
+#sudo ufw default allow outgoing
+#sudo ufw allow ssh
 
 # Cleanup
 sudo apt autoremove -y --purge
@@ -35,7 +59,8 @@ sudo apt autoclean -y
 
 echo " "
 echo "----------------------------------------"
-echo "Done. System will restart in 30seconds. To abort please stop this script with Strg+X"
+echo "Done. System will restart in 30seconds."
+echo "To abort please stop this script with Strg+X"
 echo "----------------------------------------"
 echo " "
 
